cf512b1772c0539ada6667a8a4a7b77e1978a7bc
Romj901 <jerome.laclavere.icera@gmail.com>
2016-02-02 13:37:28 +0100

    Feature: counte of mississipi

--- helloworld.c
+++ helloworld.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 int main()
 {
+int i;
  printf("Hello World ");
- for (int i=0; i>10; i++)
- 	printf("\n %d mississipi,");
+ for (i=0; i<10; i++)
+ 	printf("\n %d mississipi%c",i, (i<=1 ? ' ':'s'));
 
 }
